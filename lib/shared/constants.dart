443fd4352d70b105e6bb15d0f7e174ff33b4facf
Atharv Bhasme <atharv.bhasme@gmail.com>
2022-11-25 14:03:02 +0530

    flutter prject

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:794548982157:web:aa7db63a3269aaf7dfedc4";
+  static String apiKey = "AIzaSyA7MeBofwL2VD1SXocth48acHfHmhBTPCM";
+  static String messagingSenderId = "chatappflutter-33a12";
+  static String projectId = "chatappflutter-13318";
+  final primaryColor = const Color(0xFFee7b64);
+}
